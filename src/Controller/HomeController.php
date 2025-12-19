c9faced680de8dbcf7542d429a2a6a13fba89694
Loïs Marteel <marteellois@gmail.com>
2025-12-19 16:29:11 +0100

    Save

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Form\ContactClientFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -13,6 +16,18 @@ final class HomeController extends AbstractController
     {
         return $this->render('client/home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+    public function new(Request $request): Response
+    {
+        $message = new Message();
+        // ...
+
+        $form = $this->createForm(ContactClientFormType::class, $message);
+
+        return $this->render('client/form-contact/form.html.twig', [
+            'form' => $form,
         ]);
     }
 }
