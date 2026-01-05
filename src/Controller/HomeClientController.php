3f3bd1b74fa95a4a52b842cfde14f62c9fb1e631
Loïs Marteel <marteellois@gmail.com>
2026-01-05 15:58:34 +0100

    Rename HomeController -> HomeClientController

--- src/Controller/HomeClientController.php
+++ src/Controller/HomeClientController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class HomeClientController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
