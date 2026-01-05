54f3091428b02a21c6c1626ec39b100d19a80730
Loïs Marteel <marteellois@gmail.com>
2026-01-05 15:48:42 +0100

    Formulaire contact client - done local

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Message;
 use App\Form\ContactClientFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,15 +20,51 @@ final class HomeController extends AbstractController
         ]);
     }
 
-    public function new(Request $request): Response
+    #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
+    public function contact(Request $request, EntityManagerInterface $entityManager): Response
     {
         $message = new Message();
-        // ...
+        $success = false;
+        $errors = [];
+
+        // Si l'utilisateur est connecté, remplir automatiquement ID_user
+        if ($this->getUser()) {
+            $message->setIDUser($this->getUser());
+        }
 
         $form = $this->createForm(ContactClientFormType::class, $message);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            if (!$form->isValid()) {
+                // Récupérer les erreurs du formulaire
+                foreach ($form->getErrors(true) as $error) {
+                    $errors[] = $error->getMessage();
+                }
+            } else {
+                // Si l'utilisateur est connecté, le lier au message
+                // Sinon, le message sera anonyme (ID_user = null)
+                if ($this->getUser()) {
+                    $message->setIDUser($this->getUser());
+                }
+
+                try {
+                    $entityManager->persist($message);
+                    $entityManager->flush();
+
+                    $success = true;
+                    $this->addFlash('success', 'Votre message a été envoyé avec succès.');
+                    return $this->redirectToRoute('app_home');
+                } catch (\Exception $e) {
+                    $errors[] = 'Erreur lors de l\'envoi: ' . $e->getMessage();
+                }
+            }
+        }
 
         return $this->render('client/form-contact/form.html.twig', [
             'form' => $form,
+            'success' => $success,
+            'errors' => $errors,
         ]);
     }
 }
--- src/Form/ContactClientFormType.php
+++ src/Form/ContactClientFormType.php
@@ -3,9 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Message;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,14 +15,26 @@ class ContactClientFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Nom')
-            ->add('Prenom')
-            ->add('Email')
-            ->add('Telephone')
-            ->add('Description')
-            ->add('ID_user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
+            ->setAction('/contact')
+            ->add('Nom', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('Prenom', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('Email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('Telephone', TextType::class, [
+                'label' => 'Téléphone',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('Description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => ['class' => 'form-control', 'rows' => 5],
             ])
         ;
     }
